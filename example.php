7a0d1d2609ab0856ebb0c722e3dedc03d4c6b9ef
Max Base <MaxBaseCode@Gmail.Com>
2022-10-05 23:18:51 +0200

    Create example.php

---
+++ example.php
@@ -0,0 +1,37 @@
+<?php
+require "YourCombinations.php";
+
+// Eaxmple
+$n = 4;
+$k = [1, 2, 3];
+
+$combinations = new YourCombinations($k);
+
+foreach ($combinations->permutations($n, true) as $item) {
+	print_r($item);
+}
+
+print("=====================");
+
+foreach ($combinations->combinations($n, true) as $item) {
+	print_r($item);
+}
+
+print("=====================");
+
+foreach ($combinations->permutations(2, false) as $item) {
+	print_r($item);
+}
+
+print("=====================");
+
+foreach ($combinations->combinations(2, false) as $item) {
+	print_r($item);
+}
+
+print("=====================");
+
+foreach ($combinations->powerSet($k) as $item) {
+	print_r($item);
+}
+// print([...$combinations->powerSet($k)]);
