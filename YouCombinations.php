e09f40201c23d3f3503979d69d13c21a10d0f0f8
Max Base <MaxBaseCode@Gmail.Com>
2022-10-05 23:15:51 +0200

    Update and rename YouCombinations.php to YourCombinations.php

--- YouCombinations.php
+++
@@ -1,7 +0,0 @@
-/*
- * Name: Your Combinations PHP
- * Author: Max Base
- * Date: 2022/10/05
- * Repository: https://github.com/basemax/YourCombinationsPHP
- */
-
---
+++ YourCombinations.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ * Name: Your Combinations PHP
+ * Author: Max Base
+ * Date: 2022/10/05
+ * Repository: https://github.com/basemax/YourCombinationsPHP
+ */
+
+class Combinations {
+	private $elements = [];
+	
+	public function __construct($elements) {
+		$this->elements = array_values($elements);
+	}
+	
+	public function Combinations($length, $with_repetition = false, $position = 0, $elements = []) {
+		$items_count = count($this->elements);
+		for ($i = $position; $i < $items_count; $i++) {
+			$elements[] = $this->elements[$i];
+				if (count($elements) == $length) yield $elements;
+			else foreach ($this->x_calculateCombinations($length, $with_repetition, ($with_repetition == true ? $i : $i + 1), $elements) as $value2) yield $value2;
+			array_pop($elements);
+		}
+	}
+
+	public function Permutations($length, $with_repetition = false, $elements = [], $keys = []) {
+		foreach($this->elements as $key => $value) {
+			if ($with_repetition == false) if (in_array($key, $keys)) continue;
+			$keys[] = $key;
+			$elements[] = $value;
+			if (count($elements) == $length) yield $elements;
+			else foreach ($this->x_calculatePermutations($length, $with_repetition, $elements, $keys) as $value2) yield $value2;
+			array_pop($keys);
+			array_pop($elements);
+		}
+	}
+}
